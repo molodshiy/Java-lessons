d0a66f5ce2956838cae0d4ab7aaad06becf1ae99
vanok1993 <ivan_datsiv@mail.ru>
2014-05-05 16:06:02 +0300

    Update TryToGues.java

--- GameTryToGues/TryToGues.java
+++ GameTryToGues/TryToGues.java
@@ -6,41 +6,44 @@ import java.util.Scanner;
 
 public class Ex6TryToGues {
 
-	private static Scanner sc;
-
 	public static void main(String[] args) {
+		//Загадуємо число;
 		Random rnd = new Random();
-		int x = rnd.nextInt(6);
-		
+		int x = rnd.nextInt(6);	
 		System.out.println("Число загадане");
 		
-		sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		//Кількість спроб;
 		int m = 3;
 		
+		//Цикл для кожної спроби;
 	ww: while(m != 0){
-		
 			System.out.println("Введіть число від 0 до 6 залишилось спроб: " +m);
 			
+			//Перевіряємо введено число чи символ;
 			while(!sc.hasNextInt()){
 				System.out.println("Введіть число від 0 до 6 залишилось спроб: " +m);
 				sc.next();
 			}
 			int a = sc.nextInt();
 			
+			//Перевіряємо чи введено число в діапазоні від 0 включно до 7 виключно;
 			if(a > -1 && a < 7){
-				
+				//Умова Перемоги;
 				if(a == x){
 					System.out.println("Ви перемогли");
 					break ww;
 				}
+				//Зменшуємо кількість кроків;
 				else
 					m--;
-				
+				//Умова Програшу
 				if(m == 0){
 					System.out.println("Ви програли");
 					break ww;
 				}
 			}
+			//Повторюємо цикл при введені числа не з діапазону від 0 включно до 7 виключно;
 			else
 				continue ww;
 		}		
