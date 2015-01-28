d64b1397e026eb9834587ae5abed34d75939e98a
vanok1993 <ivan_datsiv@mail.ru>
2015-01-28 12:33:52 +0200

    Create ThreadHello.java

---
+++ Thread/Ex1Thread/ThreadHello.java
@@ -0,0 +1,13 @@
+public class ThreadHello extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello");
+        }
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
