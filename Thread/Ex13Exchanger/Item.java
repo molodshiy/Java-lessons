a03c7313a68860c8f2c945cab134e44a2c23328f
vanok1993 <ivan_datsiv@mail.ru>
2015-01-29 17:21:01 +0200

    Create Item.java

---
+++ Thread/Ex13Exchanger/Item.java
@@ -0,0 +1,27 @@
+public class Item {
+
+    private Integer id;
+    private Integer number;
+
+    public Item(Integer id, Integer number) {
+        super();
+        this.id = id;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
