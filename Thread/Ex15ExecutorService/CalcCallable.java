56bed93bca47f37bea62e21bf0f0b9a8f95bf22d
vanok1993 <ivan_datsiv@mail.ru>
2015-02-02 17:06:41 +0200

    Create CalcCallable.java

---
+++ Thread/Ex15ExecutorService/CalcCallable.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class CalcCallable implements Callable<Number> {
+
+    @Override
+    public Number call() throws Exception {
+        Number res = new Random().nextGaussian();
+        return null;
+    }
+}
