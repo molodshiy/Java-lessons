6e8b61a2613e873e48247b40fe4f2549e17b3dce
vanok1993 <ivan_datsiv@mail.ru>
2015-02-03 01:17:54 +0200

    Create SimpleJDBCRunner.java

---
+++ JDBC/Ex1DriverManager/SimpleJDBCRunner.java
@@ -0,0 +1,78 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class SimpleJDBCRunner {
+    public static void main(String[ ] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/testphones";
+        Properties prop = new Properties();
+        prop.put("user", "root");
+        prop.put("password", "root");
+        prop.put("autoReconnect", "true");
+        prop.put("characterEncoding", "UTF-8");
+        prop.put("useUnicode", "true");
+
+        Connection cn = null;
+
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+
+        try { // 1 блок
+            cn = DriverManager.getConnection(url, prop);
+            Statement st = null;
+            try { // 2 блок
+                st = cn.createStatement();
+                ResultSet rs = null;
+                try { // 3 блок
+                    rs = st.executeQuery("SELECT * FROM phonebook");
+                    ArrayList<Abonent> lst = new ArrayList<>();
+                    while (rs.next()) {
+                        int id = rs.getInt(1);
+                        int phone = rs.getInt(3);
+                        String name = rs.getString(2);
+                        lst.add(new Abonent(id, phone, name));
+                    }
+                    if (lst.size() > 0) {
+                        System.out.println(lst);
+                    } else {
+                        System.out.println("Not found");
+                    }
+                } finally { // для 3-го блока try
+/*
+* close ResultSet
+*/
+                    if (rs != null) { // был ли создан ResultSet
+                        rs.close();
+                    } else {
+                        System.err.println(
+                                "ошибка во время чтения из БД");
+                    }
+                }
+            } finally {
+/*
+* close Statement
+*/
+                if (st != null) { // для 2-го блока try
+                    st.close();
+                } else {
+                    System.err.println("Statement не создан");
+                }
+            }
+
+        } catch (SQLException e) { // для 1-го блока try
+            System.err.println("DB connection error: " + e);
+
+        } finally {
+/*
+* close Connection, if Connection is open
+*/
+            if (cn != null) {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    System.err.println("Сonnection close error: " + e);
+                }
+            }
+        }
+    }
+}
