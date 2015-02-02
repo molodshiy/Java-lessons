0c73eae54202625b09a6cb0f7312679340e36141
vanok1993 <ivan_datsiv@mail.ru>
2015-02-02 17:12:55 +0200

    Create RunExecutor.java

---
+++ Thread/Ex15_1ExecutorService/RunExecutor.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class RunExecutor {
+    public static void main(String[] args) throws Exception{
+
+        ArrayList<Future<String>> list = new ArrayList<Future<String>>();
+        ExecutorService es = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 7; i++) {
+            list.add(es.submit(new BaseCallable()));
+        }
+
+        es.shutdown();
+
+        for (Future future : list) {
+            System.out.println(future.get() +" result fixed");
+        }
+    }
+}
