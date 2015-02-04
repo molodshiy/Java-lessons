ddf2607c4217c0ab2a9f76c0b40942b4147a73cf
vanok1993 <ivan_datsiv@mail.ru>
2015-02-05 00:32:49 +0200

    Create PreparedJDBCRunner.java

---
+++ JDBC/Ex2PreparedStatement/PreparedJDBCRunner.java
@@ -0,0 +1,30 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PreparedJDBCRunner {
+    public static void main(String[] args) {
+    
+        ArrayList<Abonent> list = new ArrayList<Abonent>() {
+            {
+                add(new Abonent(87, 1658468, "GFDDD"));
+                add(new Abonent(51, 8866711, "dgfgddgfgf"));
+            }
+        };
+        
+        DataBaseHelper helper = null;
+       PreparedStatement statement = null;
+       
+        try {
+            helper = new DataBaseHelper();
+            statement = helper.getPreparedStatement();
+            for (Abonent abonent : list) {
+                helper.insertAbonent(statement, abonent);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            helper.closeStatement(statement);
+        }
+    }
+}
