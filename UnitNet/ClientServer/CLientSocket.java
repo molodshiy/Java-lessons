ab0b9a1f2bbfd016a02332dbf187dfa7c4d40b01
vanok1993 <ivan_datsiv@mail.ru>
2015-02-06 17:12:50 +0200

    Create CLientSocket.java

---
+++ UnitNet/ClientServer/CLientSocket.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class CLientSocket {
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        Socket socket = null;
+        
+        try {
+            socket = new Socket("127.0.0.1", 8030);
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = br.readLine();
+            System.out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
